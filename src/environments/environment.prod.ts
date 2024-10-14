dcda50ca2b5aa99c70b9b60476ce7d4a6197a51f
RidoBEIT <ridouane@beitniger.com>
2024-10-14 18:28:14 +0100

    14/10/2024

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    host: "http://localhost:8093/api",
+    adminUrl: "http://localhost:8093/admin",
+    vendeurUrl: "/vendeur",
+    authenticationURL: "http://localhost:8093/authenticate",
+    photoBaseUrl: "http://localHost:8093/api/rest/compagnies/getPhoto/"
+};
